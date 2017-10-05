c7f1352fe723bd5fd969374dc68685964385bae8
Pablo Gonzalez <pabloasecas@gmail.com>
2017-10-05 17:18:09 -0400

    Programmed the verb inflection corruptor

--- transformer.py
+++ transformer.py
@@ -11,7 +11,7 @@ for line in lemma_file.readlines():
 verb_list = []
 verb_file = open("verbos-espanol-conjugaciones.txt")
 for line in lemma_file.readlines():
-    lemma_list.append(line.strip())
+    verb_list.append(line.strip())
 
 
 class corruptor ():
@@ -48,35 +48,95 @@ class VerbRemover(corruptor):
 
 # Verbal inflection corruption
 class VerbInflCorruptor(corruptor):
-    main_verb_tag = re.compile("vm[is][cfips]000")
-    verb_infl_regex = re.compile("([A-Za-záéíóúñÑÁÉÍÓÚ]+)(o|s|e|)$")
+    # Version with all main verbs
+    # main_verb_tag = re.compile("vm[is][cfips]000")
+    # Version with only the three most common tenses
+    main_verb_tag = re.compile("vm[i][ips]000")
+
+    root_str = "([A-Za-záéíóúñÑÁÉÍÓÚ]+)"
+
+    # Morphological model of the present indicative
+    pres_ind_tag = "vmip000"
+    pres_infl = "(o|es|és|ás|e|emos|éis|en|as|a|amos|áis|an|imos|ís)$"
+    verb_pr_ind_regex = re.compile(root_str + pres_infl)
+
+    # Morphological model of regular past imprefect of the indicative
+    imp_ind_tag = "vmii000"
+    imperf_infl = "((aba|ía)(|s|mos|is|n))$"
+    verb_imp_ind_regex = re.compile(root_str + imperf_infl)
+
+    # Morphological model of regular simple past of the indicative
+    perf_ind_tag = "vmis000"
+    perf_infl = "(é|aste|ó|amos|asteis|aron|í|iste|ió|imos|isteis|ieron)$"
+    verb_ps_ind_regex = re.compile(root_str + perf_infl)
 
     def test_possible(self, sentence):
         token_list = sentence.tokens
         for token in token_list:
-            if token.pos == tree_handler.adj_TAG:
-                if VerbInflCorruptor.inf_ADJ_regex.matches(token.text):
-                    return True
+            if token.pos == tree_handler.VerbInflCorruptor.main_verb_tag:
+                return True
         return False
 
     def transform(self, sentence):
         token_list = sentence.tokens
+        possib = []
         for token in token_list:
-            if token.pos == tree_handler.adj_TAG:
-                if AdjInflCorruptor.inf_ADJ_regex.matches(token.text):
-                    match = AdjInflCorruptor.inf_ADJ_regex.match(token.text)
-                    root = match.group(1)
-                    infl = match.group(2)
-                    pos_inf = ["a", "o", "as", "os", "es"]
-                    pos_inf.remove(infl)
-                    for new_inf in pos_inf:
-                        if root + new_inf in lemma_list:
-                            new_adj = root + new_inf
-                            before = sentence.text[:token.beg]
-                            after = sentence.text[token.end:]
-                            newText = before + new_adj + after
-                            return newText
-        return -1
+            if token.pos == tree_handler.pres_ind_tag:
+                # Assemble the appropriate replacements
+                # Verb is in present indicative
+                if token.pos == VerbInflCorruptor.pres_ind_tag:
+                    pos_inf = ["o", "es", "és", "ás", "e", "emos", "éis", "en"
+                               , "as", "a", "amos", "áis", "an", "imos", "ís"]
+                    v_match = VerbInflCorruptor.verb_pr_ind_regex.match(token)
+                    root = v_match.group(1)
+                    infl = v_match.group(2)
+                    if infl[0] == "a" or infl[0] == "á":
+                        for i in pos_inf:
+                            if i[0] != "a" and i[0] != "o" and i[0] != "á":
+                                pos_inf.remove(i)
+                    else:
+                        for inf in pos_inf:
+                            if inf[0] == "a" or inf[0] == "á":
+                                pos_inf.remove(inf)
+                # Verb is in imperfect indicative
+                elif token.pos == VerbInflCorruptor.imp_ind_tag:
+                    v_match = VerbInflCorruptor.verb_imp_ind_regex.match(token)
+                    root = v_match.group(1)
+                    infl = v_match.group(2)
+                    mid = v_match.group(3)
+                    pos_inf = [mid, mid + "s", mid + "mos",
+                               mid+"is", mid + "n"]
+                # Verb is in past simple
+                elif token.pos == VerbInflCorruptor.perf_ind_tag:
+                    pos_inf = ["é", "aste", "ó", "amos", "asteis", "aron", "í",
+                               "iste", "ió", "imos", "isteis", "ieron"]
+                    v_match = VerbInflCorruptor.verb_ps_ind_regex.match(token)
+                    root = v_match.group(1)
+                    infl = v_match.group(2)
+                    if infl[0] == "a" or infl[0] == "á":
+                        for i in pos_inf:
+                            if i[0] != "a" and i[0] != "é" and i[0] != "á":
+                                pos_inf.remove(i)
+                    else:
+                        for inf in pos_inf:
+                            if inf[0] == "a" or inf[0] == "é":
+                                pos_inf.remove(inf)
+
+                pos_inf.remove(infl)
+                for new_inf in pos_inf:
+                    new_verb = root + new_inf
+                    if new_verb in verb_list:
+                        rep = [new_verb, token.beg, token.end]
+                        possib.append(rep)
+        if len(possib) != 0:
+            choice = random.choose(possib)
+            new_adj = choice[0]
+            before = sentence.text[:choice[1]]
+            after = sentence.text[choice[2]:]
+            newText = before + new_adj + after
+            return newText
+        else:
+            return -1
 
 
 # Adjectival inflection corruption
