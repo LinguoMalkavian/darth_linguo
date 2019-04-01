144913167d0b9afc713adf1d5190b8bb6c8a866b
Pablo Gonzalez <pablo@Pablos-MacBook-Pro.local>
2019-04-01 16:25:01 -0400

    Renamed experimen3 configs and corresponding file

---
+++ pbs_scripts/run_exp3.sh
@@ -0,0 +1,11 @@
+#Run the experiments
+set -x
+source activate allennlp
+
+experiments=(exp-3_uni-1-layer_32-32 exp-3_uni-2-layer_32-32 exp-3_uni-3-layer_32-32 exp-3_bi-1-layer_32-32 exp-3_bi-2-layer_32-32 exp-3_bi-3-layer_32-32 exp-3_uni-1-layer_32-64 exp-3_uni-2-layer_32-64 exp-3_uni-3-layer_32-64 exp-3_bi-1-layer_32-64 exp-3_bi-2-layer_32-64 exp-3_bi-3-layer_32-64 exp-3_uni-1-layer_64-64 exp-3_uni-2-layer_64-64 exp-3_uni-3-layer_64-64 exp-3_bi-1-layer_64-64 exp-3_bi-2-layer_64-64 exp-3_bi-3-layer_64-64 exp-3_uni-1-layer_64-128 exp-3_uni-2-layer_64-128 exp-3_uni-3-layer_64-128 exp-3_bi-1-layer_64-128 exp-3_bi-2-layer_64-128 exp-3_bi-3-layer_64-128 exp-3_uni-2-layer_128-128 exp-3_uni-3-layer_128-128 exp-3_uni-1-layer_128-128 exp-3_bi-1-layer_128-128 exp-3_bi-2-layer_128-128 exp-3_bi-3-layer_128-128)
+
+for exp_name in "${experiments[@]}"; 
+do
+    allennlp train /home/lab/Pablo/darth_linguo/experiments/$exp_name.json -s /home/lab/Pablo/darth_linguo/results/$exp_name --include-package allen_linguo
+    bash /home/lab/Pablo/darth_linguo/pbs_scripts/generate_predictions.sh $exp_name
+done
