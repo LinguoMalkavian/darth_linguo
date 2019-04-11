7bc1bc073b2889a934d6b2aa96a8b2bed98c3c6e
Pablo Gonzalez <pabloasecas@gmail.com>
2019-04-11 10:19:24 -0400

    naming conventions ups

--- pbs_scripts/generate_predictions.sh
+++ pbs_scripts/generate_predictions.sh
@@ -1,28 +1,28 @@
 #!/bin/bash -x
 
 set -x 
-source activate allennlp
+source activate allen 
 
-allennlp predict /home/lab/Pablo/darth_linguo/results/$1/model.tar.gz \
-/home/lab/Pablo/darth_linguo/Data/exp3-pairtest/exp3-pairtest_AA \
+allennlp predict /home/lab/home/lab/Pablo/darth_linguo/results/$1/model.tar.gz \
+/home/lab/home/lab/Pablo/darth_linguo/Data/exp3-pairtest/exp3-pairtest_AA \
 --include-package allen_linguo \
 --cuda-device 0 \
---weights-file /home/lab/Pablo/darth_linguo/results/$1/best.th \
---output-file /home/lab/Pablo/darth_linguo/results/pairtest/classifier_results/$1_AA_results \
+--weights-file /home/lab/home/lab/Pablo/darth_linguo/results/$1/best.th \
+--output-file /home/lab/home/lab/Pablo/darth_linguo/results/pairtest/classifier_results/$1_AA_results \
 --use-dataset-reader --predictor linguo-predictor  --silent
 
-allennlp predict /home/lab/Pablo/darth_linguo/results/$1/model.tar.gz \
-/home/lab/Pablo/darth_linguo/Data/exp3-pairtest/exp3-pairtest_VA \
+allennlp predict /home/lab/home/lab/Pablo/darth_linguo/results/$1/model.tar.gz \
+/home/lab/home/lab/Pablo/darth_linguo/Data/exp3-pairtest/exp3-pairtest_VA \
 --include-package allen_linguo \
 --cuda-device 0 \
---weights-file /home/lab/Pablo/darth_linguo/results/$1/best.th \
---output-file /home/lab/Pablo/darth_linguo/results/pairtest/classifier_results/$1_VA_results \
+--weights-file /home/lab/home/lab/Pablo/darth_linguo/results/$1/best.th \
+--output-file /home/lab/home/lab/Pablo/darth_linguo/results/pairtest/classifier_results/$1_VA_results \
 --use-dataset-reader --predictor linguo-predictor --silent
  
-allennlp predict /home/lab/Pablo/darth_linguo/results/$1/model.tar.gz \
-/home/lab/Pablo/darth_linguo/Data/exp3-pairtest/exp3-pairtest_RV \
+allennlp predict /home/lab/home/lab/Pablo/darth_linguo/results/$1/model.tar.gz \
+/home/lab/home/lab/Pablo/darth_linguo/Data/exp3-pairtest/exp3-pairtest_RV \
 --include-package allen_linguo \
 --cuda-device 0 \
---weights-file /home/lab/Pablo/darth_linguo/results/$1/best.th \
---output-file /home/lab/Pablo/darth_linguo/results/pairtest/classifier_results/$1_RV_results \
+--weights-file /home/lab/home/lab/Pablo/darth_linguo/results/$1/best.th \
+--output-file /home/lab/home/lab/Pablo/darth_linguo/results/pairtest/classifier_results/$1_RV_results \
 --use-dataset-reader --predictor linguo-predictor --silent
--- pbs_scripts/run_exp3.sh
+++ pbs_scripts/run_exp3.sh
@@ -1,6 +1,6 @@
 #Run the experiments
 set -x
-source activate allennlp
+source activate allen
 
 experiments=(exp-3_uni-1-layer_32-32 exp-3_uni-2-layer_32-32 exp-3_uni-3-layer_32-32 exp-3_bi-1-layer_32-32 exp-3_bi-2-layer_32-32 exp-3_bi-3-layer_32-32 exp-3_uni-1-layer_32-64 exp-3_uni-2-layer_32-64 exp-3_uni-3-layer_32-64 exp-3_bi-1-layer_32-64 exp-3_bi-2-layer_32-64 exp-3_bi-3-layer_32-64 exp-3_uni-1-layer_64-64 exp-3_uni-2-layer_64-64 exp-3_uni-3-layer_64-64 exp-3_bi-1-layer_64-64 exp-3_bi-2-layer_64-64 exp-3_bi-3-layer_64-64 exp-3_uni-1-layer_64-128 exp-3_uni-2-layer_64-128 exp-3_uni-3-layer_64-128 exp-3_bi-1-layer_64-128 exp-3_bi-2-layer_64-128 exp-3_bi-3-layer_64-128 exp-3_uni-2-layer_128-128 exp-3_uni-3-layer_128-128 exp-3_uni-1-layer_128-128 exp-3_bi-1-layer_128-128 exp-3_bi-2-layer_128-128 exp-3_bi-3-layer_128-128)
 
